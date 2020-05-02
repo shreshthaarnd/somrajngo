083f3f2122ea65ce644a61cddc9e9bc8946ab4b2
unknown <sonalikul1999@gmail.com>
2020-05-02 18:46:21 +0530

    sonali commit1

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def about(request):
+	return render(request,'about.html',{})
+def blog(request):
+	return render(request,'blog.html',{})
+def blogsingle(request):
+	return render(request,'blog-single.html',{})
+def causes(request):
+	return render(request,'causes.html',{})
+def contact(request):
+	return render(request,'contact.html',{})
+def index(request):
+	return render(request,'index.html',{})
+def services(request):
+	return render(request,'services.html',{})
--- somrajngo/urls.py
+++ somrajngo/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/',about),
+    path('blog/',blog),
+    path('blogsingle/',blogsingle),
+    path('causes/',causes),
+    path('contact/',contact),
+    path('index/',index),
+    path('services/',services),
 ]
