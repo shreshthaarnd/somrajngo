61d7821aac75f20e9eca69d36f4c6b252273456d
aryankul97 <aryankul97@gmail.com>
2020-05-03 20:35:27 +0530

    aryan commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import *
+from django.core.mail import EmailMessage
 
-# Create your views here.
 def about(request):
 	return render(request,'about.html',{})
 def blog(request):
@@ -15,7 +16,51 @@ def index(request):
 	return render(request,'index.html',{})
 def services(request):
 	return render(request,'services.html',{})
+
+#AdminPannel Code
 def adminindex(request):
 	return render(request,'adminpages/index.html',{})
 def adminpages404withoutmenus(request):
 	return render(request,'adminpages/pages-404-withoutmenus.html',{})
+def adminlogin(request):
+	return render(request,'adminpages/adminlogin.html',{})
+
+@csrf_exempt
+def adminpanel(request):
+	if request.method=='POST':
+		e=request.POST.get('adminid')
+		p=request.POST.get('pass')
+		if e=='admin@ngo.com' and p=='1234':
+			request.session['admin_id'] = e
+			return render(request,'adminpages/index.html',{})
+		else:
+			return render(request,'adminpages/adminlogin.html',{'msg':'Incorrect ID or Password'})
+	else:
+		return redirect('/error404/')
+
+def adminhome(request):
+	try:
+		if request.session['admin_id'] == 'admin@ngo.com':
+			return render(request,'adminpages/postnews.html',{})
+		else:
+			return redirect('/error404/')
+	except:
+		return redirect('/error404/')
+
+@csrf_exempt
+def adminlogout(request):
+	try:
+		del request.session['admin_id']
+		request.session.flush()
+		return redirect('/adminlogin/')
+	except:
+		return redirect('/error404/')
+
+def postnews(request):
+	#try:
+	#	if request.session['admin_id'] == 'admin@ngo.com':
+			return render(request,'adminpages/postnews.html',{})
+	#	else:
+	#		return redirect('/error404/')
+	#except:
+	#	return redirect('/error404/')
