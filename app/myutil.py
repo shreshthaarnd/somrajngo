b01ffdbebd0b39df95b59b39cf6766a47d61b34d
aryankul97 <aryankul97@gmail.com>
2020-05-19 00:02:10 +0530

    first commit

--- app/myutil.py
+++ app/myutil.py
@@ -1,4 +1,5 @@
 from app.models import *
+from django.core.mail import EmailMessage
 
 def GetUserDashboard(uid):
 	dic={}
@@ -76,3 +77,35 @@ def GetHomeCampaigns():
 	lt2=list(reversed(lt))
 	dic={'cam1':lt2[0:1], 'cam2':lt2[1:2], 'cam3':lt2[2:4]}
 	return dic
+
+def sendcampaigns(emails, camid):
+	obj=CampaignData.objects.filter(Campaign_ID=camid)
+	for x in obj:
+		if x.Campaign_Title != 'NA':
+			obj1=UserData.objects.filter(User_ID=x.User_ID)
+			for y in obj1:
+				msg='''Greetings from Aaeena!!!!
+'''+y.User_Fname+''' '''+y.User_Lname+''' is sending you his campaign details.
+Campaign Name : '''+x.Campaign_Title+'''
+About Campaign : '''+x.Campaign_About+'''
+
+You can also contact him at : '''+y.User_Phone+''' or '''+y.User_Email+'''
+
+Need Donation? : '''+x.Campaign_Donation+'''
+
+Bank Details :
+Account Holder's Name : '''+x.Campaign_Account_Name+'''
+Account Number : '''+x.Campaign_Account_Number+'''
+IFSC Code : '''+x.Campaign_Account_IFSC+'''
+Bank Name : '''+x.Campaign_Account_Bank+'''
+
+Please help them as much as you can.
+
+Thank You!
+Team Aaeena'''
+				sub='Aaeena - Campaign (A request to help)'
+				for z in emails[0:50]:
+					email=EmailMessage(sub,msg,to=[z])
+					email.send()
+					print('Processing....')
+	return True
